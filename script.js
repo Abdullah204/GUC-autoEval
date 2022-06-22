0e93e11ca9586e1f58c0d0c0f82c0341d1bbc737
Abdullah204 <57245606+Abdullah204@users.noreply.github.com>
2022-06-22 20:35:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function evaluate(x) {
+    var radios = document.getElementsByTagName('input');
+        var c = 0;
+        for (i = 0; i < radios.length; i++) {
+            
+            if (radios[i].type == 'radio') {
+                if(c%6===x)
+                    radios[i].checked = true;
+                c+=1;
+            }
+        }
+    document.getElementsByName("pstEvalBtn")[0].click();
+  }
+  var x = parseInt(prompt("enter number from 0 to 5 to choose from strongly agree (0) to strongly disagree(5) or -1 to cancel"));
+  if(x>-1){
+    evaluate(x);
+  }
